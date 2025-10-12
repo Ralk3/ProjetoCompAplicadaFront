c9a6318a21f97d8f664115c50c52c593c25dc8c0
Hiago Venancio <hiagovenancio@uni9.edu.br>
2025-10-12 16:06:46 -0300

    Commit 1: Estrutura inicial da tela de cadastro

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,27 @@
-//teste 2
-
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import HeaderPublic from '../../components/HeaderPublic.jsx'
+import './Register.css'
 
 export default function Register() {
-  return <h1>Cadastro</h1>
+  return (
+    <div className="sf-register">
+      <HeaderPublic />
+
+      <div className="sf-register__pagehead">
+        <div className="sf-register__pagehead-inner">
+          <div className="sf-register__titles">
+            <h1>Criar conta</h1>
+            <p>Comece gratuitamente publicando ou contratando serviços</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="sf-register__container">
+        <div className="sf-register_card sf-register_col">
+          <p>Conteúdo inicial aqui...</p>
+        </div>
+      </div>
+    </div>
+  )
 }
