e01c5c630bbed0c54e5c6ce7471306925b4495cf
Juu <130713989+OliJuv@users.noreply.github.com>
2025-11-26 20:47:39 -0300

    gerenciamento de pagamentos
    Added state management and imports for payments functionality.

--- src/pages/Payments/Payments.jsx
+++ src/pages/Payments/Payments.jsx
@@ -1,5 +1,33 @@
-import React from 'react'
+import React,{ useEffect, useState } from "react";
+import{Button} from "@/components/ui/button";
+import{Card, CardContent} from "@/components/ui/card";
+import{inpu} from "@/components/ui/input";
 
-export default function Payments() {
-  return <h1>Pagamentos</h1>
+import{
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+}from "@/components/ui/select";
+
+import{
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+}from "@/components/ui/dialog";
+import{motion}from "framer-motion";
+
+export default function Payments(){
+  const [payments, setPayments]=useState([]);
+  const [open, setOpen]=useState(false);
+  const [newPayment, setNewPayment]=useState({
+    name:"",
+    value:"",
+    method:"Pix",
+  });
 }
+const [loading, setLoading]= useState(falsse);
+const API_URL = "http://localhost:3000/payments";
