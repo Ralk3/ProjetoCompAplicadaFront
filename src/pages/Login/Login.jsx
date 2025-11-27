44d621721e8d157793dc6b449c9b68e9b8f3d655
Victor Gabriel Passos de Souza <victopassos@uni9.edu.br>
2025-11-26 21:38:56 -0300

    segundo commit: login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeaderPublic from '../../components/HeaderPublic.jsx'
 import './Login.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className="sf-login">
       <HeaderPublic />
@@ -17,8 +20,39 @@ export default function Login() {
       </div>
 
       <div className="sf-login__container">
-        <div className="sf-login_card sf-login_col"></div>
-        <div className="sf-login_card sf-login_col"></div>
+        <div className="sf-login__card sf-login__col"></div>
+
+        <div className="sf-login__card sf-login__col">
+          <h2 className="sf-login__formtitle">Bem-vindo de volta</h2>
+
+          <form className="sf-login__form">
+            <label className="sf-login__label">E-mail</label>
+            <div className="sf-login__field">
+              <span className="sf-login__icon">✉️</span>
+              <input
+                className="sf-login__input"
+                type="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <label className="sf-login__label" style={{ marginTop: 12 }}>Senha</label>
+            <div className="sf-login__field">
+              <span className="sf-login__icon">🔒</span>
+              <input
+                className="sf-login__input"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button className="sf-login__btn">Entrar</button>
+          </form>
+        </div>
       </div>
 
       <div className="sf-login__footerbar">
