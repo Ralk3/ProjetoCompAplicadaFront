ea7bbc89454128cfea9242418f6a2888279e52e8
Victor Gabriel Passos de Souza <victopassos@uni9.edu.br>
2025-11-26 21:27:51 -0300

    Estrutura inicial

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,29 @@
-
 import React from 'react'
+import HeaderPublic from '../../components/HeaderPublic.jsx'
+import './Login.css'
 
 export default function Login() {
-  return <h1>Login</h1>
+  return (
+    <div className="sf-login">
+      <HeaderPublic />
+
+      <div className="sf-login__pagehead">
+        <div className="sf-login__pagehead-inner">
+          <div className="sf-login__titles">
+            <h1>Entrar</h1>
+            <p>Acesse sua conta para continuar.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="sf-login__container">
+        <div className="sf-login_card sf-login_col"></div>
+        <div className="sf-login_card sf-login_col"></div>
+      </div>
+
+      <div className="sf-login__footerbar">
+        <a className="sf-login__privacy">Política de privacidade</a>
+      </div>
+    </div>
+  )
 }
